ac036ab11c9847f75b8f785c74cfe6d9f067c781
acdominguez03 <acordondominguez31@gmail.com>
2023-01-24 14:08:41 +0100

    Init cards and collections

---
+++ app/Http/Controllers/CardsController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Helpers\ResponseGenerator;
+use Illuminate\Support\Facades\Validator;
+use App\Models\Card;
+
+class CardsController extends Controller
+{
+    //
+}
---
+++ database/migrations/2023_01_24_130314_create_collections_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::dropIfExists('collections');
+        Schema::create('collections', function (Blueprint $table) {
+            $table->id();
+            $table->text('name');
+            $table->text('symbol');
+            $table->timestamp('editDate');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('collections');
+    }
+};
